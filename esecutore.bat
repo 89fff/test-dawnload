e067605cc13100965f66a5c40ae3b80ef9f29db2
89fff <88288666+89fff@users.noreply.github.com>
2026-06-26 13:05:10 +0200

    Update esecutore.bat

--- esecutore.bat
+++ esecutore.bat
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 javac index.java
 java index
-start "" "C:\Users\User\Downloads\test_download\file_bat\test.bat"
+start "" "%userprofile%\downloads\test_download\file_bat\test.bat"
 pause
